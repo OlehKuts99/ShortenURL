a450faecd595f7c11fd985aea2dacbfc401ef273
Oleh Kuts <47148596+OlehKuts99@users.noreply.github.com>
2021-12-09 21:34:20 +0200

    Initial commit

---
+++ Helpers/ShortLinkHelper.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.WebUtilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UrlShortener.Helpers
+{
+    public class ShortLinkHelper
+    {
+        public string GetUrlChunk(int id)
+        {
+            return WebEncoders.Base64UrlEncode(BitConverter.GetBytes(id));
+        }
+
+        public int GetId(string urlChunk)
+        {
+            return BitConverter.ToInt32(WebEncoders.Base64UrlDecode(urlChunk));
+        }
+    }
+}
