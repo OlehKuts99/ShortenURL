a4a2f9a542a17f652e4544110c5cb2c89864c049
Oleh Kuts <47148596+OlehKuts99@users.noreply.github.com>
2021-12-09 21:36:23 +0200

    Removed before pushing

--- Models/ShortLink.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using Microsoft.AspNetCore.WebUtilities;
-
-public class ShortLink
-{
-    public int Id { get; set; }
-
-    public string Url { get; set; }
-}
--- README.md
+++
@@ -1,1 +0,0 @@
-# ShortenURL
